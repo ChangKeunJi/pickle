212149e4cf9b088e60844be2a55264c9baafd887
changkeunji <ckdrmsdk@gmail.com>
2021-10-31 06:54:00 +0900

    명령어 수정

--- pickle-front/pages/api/user.js
+++ pickle-front/pages/api/user.js
@@ -1,8 +1,6 @@
 import { getSession } from "next-auth/client";
-import { useRouter } from "next/router";
 
 const handler = async (req, res) => {
-  const router = useRouter();
   const session = await getSession({ req });
   if (session) {
     res.send(session);
