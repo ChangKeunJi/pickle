a1394e2516ef00662430774bd3ec817d6f9c93ab
changkeunji <ckdrmsdk@gmail.com>
2021-11-02 01:12:38 +0900

    aws 버그수정

--- pickle-front/pages/api/login.js
+++ pickle-front/pages/api/login.js
@@ -1,7 +1,10 @@
+import { Cookies } from "react-cookie";
+
 export default function handler(req, res) {
   const sid = req.query.sid;
-  res.setHeader("Set-Cookie", ["connect.sid=" + sid], {
-    path: "/",
-  });
+  // res.setHeader("Set-Cookie", ["connect.sid=" + sid], {
+  //   path: "/",
+  // });
+  Cookies.set("connect.sid", sid, { path: "/" });
   res.redirect("/");
 }
