f4ffa41f77420f2fd8a594956772905182c71a9c
changkeunji <ckdrmsdk@gmail.com>
2021-11-02 19:33:16 +0900

    aws 버그수정

--- pickle-front/pages/api/login.js
+++ pickle-front/pages/api/login.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   const sid = req.query.sid;
   res.setHeader("Set-Cookie", ["connect.sid=" + sid + ";" + "path=/;"]);
   res.setHeader("Set-Cookie", [
-    "connect.sid=" + "s%3A" + encodeURIComponent(sid) + ";" + "path=/;",
+    "connect.sid2=" + "s%3A" + encodeURIComponent(sid) + ";" + "path=/;",
   ]);
   res.redirect("/");
 }
