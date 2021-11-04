7a44747ec83c66784517ba511c2b32b445f08a06
changkeunji <ckdrmsdk@gmail.com>
2021-11-04 14:21:38 +0900

    aws 문제

--- pickle-front/pages/directory/[id].js
+++ pickle-front/pages/directory/[id].js
@@ -14,40 +14,34 @@ const DirectoryPosts = () => {
 };
 
 export const getServerSideProps = wrapper.getServerSideProps(
-  async (context) => {
-    const cookie = context.req ? context.req.headers.cookie : "";
+  (store) => async (req, res, next) => {
+    const cookie = req.req ? req.req.headers.cookie : "";
     axios.defaults.headers.Cookie = "";
-    if (context.req && cookie) {
+    if (req.req && cookie) {
       axios.defaults.headers.Cookie = cookie;
     }
-    // (store) => async (req, res, next) => {
-    //   const cookie = req.req ? req.req.headers.cookie : "";
-    //   axios.defaults.headers.Cookie = "";
-    //   if (req.req && cookie) {
-    //     axios.defaults.headers.Cookie = cookie;
-    //   }
 
-    context.store.dispatch({
+    store.dispatch({
       type: LOAD_MY_INFO_REQUEST,
     });
 
-    context.store.dispatch({
+    store.dispatch({
       type: LOAD_DIR_REQUEST,
     });
 
-    context.store.dispatch({
+    store.dispatch({
       type: LOAD_POST_REQUEST,
     });
 
-    context.store.dispatch({
+    store.dispatch({
       type: LOAD_DIR_POST_REQUEST,
-      data: context.req.params.id,
+      data: req.params.id,
     });
 
-    context.store.dispatch(END);
-    await context.store.sagaTask.toPromise();
+    store.dispatch(END);
+    await store.sagaTask.toPromise();
 
-    const { user } = context.store.getState();
+    const { user } = store.getState();
     if (!user.me) {
       return {
         redirect: {
